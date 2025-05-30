632f943d973f915f9e2b8948ae88cf480bd09d6c
sapna subedi <sapnasubedi@mac.myfiosgateway.com>
2025-05-30 18:29:22 -0400

    add My bank section

--- my-bank/components/RightSidebar.tsx
+++ my-bank/components/RightSidebar.tsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
 
-const RightSidebar = ({user, transactions, banks}: RightSidebarProps) => {
+const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
-    <aside className='right-sidebar'>
+    <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
         <div className="profile-banner">
-            <div className="profile">
-                <div className="profile-img">
-                    <span className='text-5xl font-bold text-blue-500'>
-                        {user.firstName[0]}
-                    </span>
-                </div>
-                <div className="profile-details">
-                    <h1 className='profile-name'>{user.firstName} {user.lastName}</h1>
-                    <p className='profile-email'>{user.email}</p>
-                </div>
-               
+          <div className="profile">
+            <div className="profile-img">
+              <span className="text-5xl font-bold text-blue-500">
+                {user.firstName[0]}
+              </span>
             </div>
+            <div className="profile-details">
+              <h1 className="profile-name">
+                {user.firstName} {user.lastName}
+              </h1>
+              <p className="profile-email">{user.email}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="banks">
+        <div className="flex w-full justify-between">
+          <h2 className="header-2">My Banks</h2>
+          <Link href="/" className="flex gap-2">
+            <Image src="/my-bank/public/Plus_symbol.svg.png" alt="/" height={20} width={20} />
+            <h2 className="text-14 font-semibold text-gray-600">Add Banks</h2>
+          </Link>
         </div>
       </section>
     </aside>
-  )
-}
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
