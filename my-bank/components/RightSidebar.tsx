76a48dd6c004f439db87bade375884158ed25741
sapna subedi <sapnasubedi@mac.myfiosgateway.com>
2025-05-30 18:34:57 -0400

    add banks

--- my-bank/components/RightSidebar.tsx
+++ my-bank/components/RightSidebar.tsx
@@ -30,6 +30,18 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             <h2 className="text-14 font-semibold text-gray-600">Add Banks</h2>
           </Link>
         </div>
+        {banks?.length >0 && (
+            <div className=" relative flex flex-1 flex-col items-center justify-center gap-5">
+                <div className="relative z-10">
+                    Bank Card 1
+                </div>
+                {banks[1] && (
+                    <div className="absolute right-0 top-8 z-0 w-[90%]">
+                        Bank Card 2
+                    </div>
+                )}
+            </div>
+        )}
       </section>
     </aside>
   );
